3a051a46221ec718913262c94e0cffa28f7459e3
Alexandre Couedelo <nokwebspace@gmail.com>
2024-11-10 14:31:23 +0000

    chore: implement unit tests properly for uppercaseStreams

--- src/test/java/com/xnok/java_kafka_streams_demo/UpperCaseStreamsTopologyTest.java
+++ src/test/java/com/xnok/java_kafka_streams_demo/UpperCaseStreamsTopologyTest.java
@@ -27,23 +27,20 @@ public class UpperCaseStreamsTopologyTest {
     public void setup() {
         // Manually create the StreamsBuilder and topology so we don't need SpringBoot for unit test
         StreamsBuilder builder = new StreamsBuilder();
-        UpperCaseStreamsTopology topologyConfig = new UpperCaseStreamsTopology();
-        topologyConfig.register(builder);
+        UpperCaseStreamsTopology upperCaseStreamsTopology = new UpperCaseStreamsTopology();
+        upperCaseStreamsTopology.register(builder);
         Topology topology = builder.build();
 
         // Manually create properties (mimicking application.properties)
         Map<String, Object> props = new HashMap<>();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test-uppercase-topology");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         Properties testConfig = new KafkaStreamsConfiguration(props).asProperties();
-        TopologyTestDriver testDriver = new TopologyTestDriver(topology, testConfig);
 
-        inputTopic = testDriver.createInputTopic("input-topic", new StringSerializer(), new StringSerializer());
-        outputTopic = testDriver.createOutputTopic("output-topic", new StringDeserializer(), new StringDeserializer());
+        testDriver = new TopologyTestDriver(topology, testConfig);
+        inputTopic = testDriver.createInputTopic(UpperCaseStreamsTopology.SOURCE, new StringSerializer(), new StringSerializer());
+        outputTopic = testDriver.createOutputTopic(UpperCaseStreamsTopology.SINK, new StringDeserializer(), new StringDeserializer());
     }
 
 
@@ -55,7 +52,9 @@ public class UpperCaseStreamsTopologyTest {
     @Test
     public void shouldConvertToUpperCase() {
         inputTopic.pipeInput("key1", "hello");
-
         assertThat(outputTopic.readValue()).isEqualTo("HELLO");
+
+        inputTopic.pipeInput("key2", "123, 953, ABC");
+        assertThat(outputTopic.readValue()).isEqualTo("123, 953, ABC");
     }
 }
